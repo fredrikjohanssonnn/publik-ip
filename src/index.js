13708dffe1d5000de077fcea5fcdfab064015629
Fredrik Johansson <fjohanssondev@gmail.com>
2022-08-24 10:34:29 +0000

    Update index.js and styles.css

--- src/index.js
+++ src/index.js
@@ -18,8 +18,9 @@ fetchIP();
 document.getElementById("app").innerHTML = `
 <main>
   <div class="container">
-  <h1>Din publika IP-adress är:</h1>
-  <div class="ip">${isLoading && "Laddar..."}</div>
+    <h1>Din publika IP-adress är:</h1>
+    <div class="ip">${isLoading && "Laddar..."}</div>
+    <p class="vpn-info">PS. Kom ihåg att stänga av din VPN-klient om du använder en sådan, t.ex. för att komma åt ett intranät på jobbet. Annars kommer inte IP-adressen du ser att vara din egna.</p>
   </div>
 </main>
 `;
